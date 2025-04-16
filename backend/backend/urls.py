f163f8efba8289b02004718b4ed01fb122df0734
Ziyad <Ziyad.M.Alshareef@gmail.com>
2025-04-17 01:13:55 +0300

    make it better

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,10 +16,12 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='frontend/index.html')),
 ]
