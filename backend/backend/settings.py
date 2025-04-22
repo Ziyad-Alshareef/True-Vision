5af5d8b960e987c2589e83a8065498b0f66c2136
Ziyad <Ziyad.M.Alshareef@gmail.com>
2025-04-23 00:21:30 +0300

    connect to supabase

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,16 +93,20 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
 
-
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
